5a9fe0a7e106c34ccef9cfcba1acc80fa6468617
andresquito <ricardoandre1@hotmail.com>
2024-03-27 15:21:46 -0500

    xs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main className="h-screen bg-orange-300">
       {/* home */}
       <div
-        className="bg-green-200 h-full flex bg-cover bg-center pr-3 pl-3 md:pl-20 md:pr-40 lg:pl-40 lg:pr-80"
+        className="bg-green-200 h-full flex bg-cover bg-center text-center pr-3 pl-3 md:pl-20 md:pr-40 lg:pl-40 lg:pr-80"
         style={{ backgroundImage: "url(/main-bg.webp)" }}
       >
         <div className="flex flex-col  ">
@@ -15,7 +15,7 @@ export default function Home() {
             src="/andres5.png"
             //voltear imagen
             style={{ transform: "scalex(-1)" }}
-            className="left-20 w-40 rounded-full md:pt-40 md:left-40"
+            className="left-20 w-30 rounded-full pt-10 md:pt-40 md:left-40"
           />
           <h1 className="text-[20px] text-white font-semibold ">
             Andres Yepez
@@ -23,13 +23,13 @@ export default function Home() {
           <h2 className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-50 text-[30px] font-bold pb-3">
             Sofware develoment
           </h2>
-          <p className="text-white pb-3">
+          <p className="text-white pb-5">
             Passionate and highly motivated in the field of software
             development, knowledge of Html, Css, JavaScript, TypeScript, React,
             Next.js, Node.js, Mongo, Express, SQL,Tailwind, Git, GitHub and
             Agile Methodologies.
           </p>
-          <div className="text-white flex flex-wrap text-center gap-3  md:flex-row">
+          <div className="text-white flex flex-wrap text-center justify-center gap-3  md:flex-row">
             <Link
               href="/nav"
               className="bg-blue-500 w-30 py-1 px-3 rounded-xl hover:bg-blue-400"
