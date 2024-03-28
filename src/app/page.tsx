31f321f63086c13ce4ecc8940180c2ff24a9372e
andresquito <ricardoandre1@hotmail.com>
2024-03-27 21:20:15 -0500

    photo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,34 +5,35 @@ export default function Home() {
   return (
     <>
       <SlideBar />
-      <main className="h-screen bg-orange-300">
+      <main className="h-screen ">
         {/* home */}
         <div
-          className="bg-green-200 h-full flex text-right bg-cover bg-center  pr-10 pl-10 sm:pl-20 sm:pr-20 md:left-20 md:pr-40 lg:pl-40 lg:pr-80"
+          className="h-full flex text-right bg-cover bg-center  pr-10 pl-10 sm:items-center sm:pl-20 sm:pr-20 md:left-20 md:pr-40 lg:pl-40 lg:pr-80 lg:text-center "
           style={{ backgroundImage: "url(/main-bg.webp)" }}
         >
-          <div className="flex flex-col ">
-            <div className="pl-40">
+          <div className="flex flex-col  w-full items-center ">
+
               <img
                 src="/andres5.png"
                 //voltear imagen
                 // style={{ transform: "scalex(-1)" }}
-                className=" w-40  rounded-full pt-40 pb-5 sm:w-60 sm:pt-40  md:pt-40 md:left-40"
+                className=" w-40  rounded-full pt-40 pb-5 sm:w-60 sm:pt-0  md:pt-0 md:left-40 lg:pt-0 "
               />
-            </div>
 
-            <h1 className="text-[20px] text-white font-semibold lg:text-[40px]">
+
+            <h1 className="text-[20px] text-white font-semibold lg:text-[40px] ">
               Andrés Yépez
             </h1>
             <h2 className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-50 text-[30px] font-bold pb-5 lg:text-[50px]">
               Sofware developer
             </h2>
-            <p className="text-white pb-5  lg:text-[20px] lg:pb-10">
+            <p className="text-white pb-5  lg:text-[20px] lg:pb-10 md:text-center ">
               Motivated in software development. <br/>I work with HTML, CSS,
               JavaScript, TypeScript, React, Next.js, SQL, Tailwind, Git and
               GitHub.
             </p>
           </div>
+
         </div>
         {/*image*/}  
         <div>
@@ -41,7 +42,7 @@ export default function Home() {
           alt="stars" 
           height={300}
           width={300}
-          className="absolute top-20 right-0 z-[10]"
+          className="absolute top-20 right-0 z-[10] "
           />          
         </div>    
       </main>
