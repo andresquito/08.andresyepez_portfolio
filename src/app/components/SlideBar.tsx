c04d1e1adb3138a71606ad1550d698f36a98ad7c
andresquito <ricardoandre1@hotmail.com>
2024-03-28 19:55:34 -0500

    href

--- src/app/components/SlideBar.tsx
+++ src/app/components/SlideBar.tsx
@@ -9,49 +9,43 @@ import { useRouter } from "next/navigation";
 // import resumePDF from "../components/resume.pdf"
 
 const SlideBar = () => {
-  const router = useRouter();
-
-  const handleClick = ()=>{
-    router.push("/skills")
-}
-
-  
 
   const links = [
     {
       id: 1,
       child: (
-        <div onClick={() => router.push("/")} className="cursor-pointe flex gap-5 ">
-          <FaHome size={25}/> Home
-        </div>
+        <>
+          <FaHome size={25} className="cursor-pointer"/> Home
+        </>
       ),
-      href: "/",
+      href: "https://08-andresyepez-portfolio.vercel.app/",
     },
     {
       id: 2,
       child: (
-        <div onClick={() => router.push("/studies")} className="cursor-pointe flex gap-5 ">
-          <FaBookOpenReader size={25} /> Studies
-        </div>
+        <>
+          <FaBookOpenReader size={25} className="cursor-pointer" /> Studies
+        </>
       ),
-      href: "/studies",
+      href: "/",
     },
     {
       id: 3,
       child: (
-        <div onClick={() => router.push("/skills")} className="cursor-pointe flex gap-5 ">
-          <FaTools size={25}  />
+        <>
+          <FaTools size={25} className="cursor-pointer" />
           Skills
-        </div>
+        </>
       ),
+      href: "https://08-andresyepez-portfolio.vercel.app/skills"
     },
     {
       id: 4,
       child: (
-        <div onClick={() => router.push("/projects")} className="cursor-pointe flex gap-5 ">
-          <FaCode size={25} />
+        <>
+          <FaCode size={25} className="cursor-pointer"/>
           Projects
-        </div>
+        </>
       ),
       href: "/",
       style: "rounded-br-md",
@@ -60,10 +54,10 @@ const SlideBar = () => {
     {
       id: 5,
       child: (
-        <div onClick={() => router.push("/contact")} className="cursor-pointe flex gap-5 ">
-          <MdMarkEmailUnread size={25} />
+        <>
+          <MdMarkEmailUnread size={25} className="cursor-pointer"/>
           Contact
-        </div>
+        </>
       ),
       href: "/",
       style: "rounded-br-md",
@@ -76,7 +70,7 @@ const SlideBar = () => {
       {links.map(({ id, child, href, style, download }) => (
         <div key={id} className={"right-0 mt-4 flex duration-300 hover:mr-4 "}>
           <a
-            onClick={handleClick}
+            href={href}
             className="flex justify-between items-center w-full text-white gap-3"
             download={download}
             target="_self"
