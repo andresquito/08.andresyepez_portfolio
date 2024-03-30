bc5166220a3a956f4b38f92f8553277c3d9f2db5
andresquito <ricardoandre1@hotmail.com>
2024-03-30 09:13:59 -0500

    bottom - h

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -8,7 +8,7 @@ const page = () => {
     className='w-screen h-screen bg-cover bg-center flex items-center justify-center'    >
         <div
         style={{backgroundImage:'url(atombg-comp.webp)'}}
-        className='w-[80%] h-[40%] md:h-[60%] relative bg-cover bg-center rounded-xl border border-white mt-40 md:mt-10'>
+        className='w-[80%] h-[50%] md:h-[60%] relative bg-cover bg-center rounded-xl border border-white mt-40 md:mt-10'>
             <div className='absolute left-5 md:left-20 bottom-10 md:bottom-36 w-[70%] md:w-[30%] '>
                 <Form/>
             </div>
